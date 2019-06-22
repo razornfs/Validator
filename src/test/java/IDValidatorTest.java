fe682eda6bf4255eed2a83e085c4fd513a295f09
Bonkers <kubaorz1@gmail.com>
2019-06-22 09:45:12 +0200

    added IDValidator tests

--- src/test/java/IDValidatorTest.java
+++ src/test/java/IDValidatorTest.java
@@ -1,4 +1,40 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class IDValidatorTest {
+
+    private Validator validator = new IDValidator();
+
+    @Test
+    public void test_higher_length() {
+        String ID = "ABC1234567";
+        assertFalse(validator.validate(ID));
+    }
+
+    @Test
+    public void test_lower_length() {
+        String ID = "ABC12345";
+        assertFalse(validator.validate(ID));
+    }
+
+    @Test
+    public void test_ID_not_starting_with_3_letters() {
+        String ID = "A0A000000";
+        assertFalse(validator.validate(ID));
+    }
+
+    @Test
+    public void test_ID_not_ending_with_6_numbers() {
+        String ID = "AAA0A0A0A";
+        assertFalse(validator.validate(ID));
+    }
+
+    @Test
+    public void test_correct_ID() {
+        String ID = "OLH281658";
+        assertTrue(validator.validate(ID));
+    }
+
+
 }
