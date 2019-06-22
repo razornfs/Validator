4fe51f4ed54f52f62b609c56ac44ed6fda0561a0
Bonkers <kubaorz1@gmail.com>
2019-06-22 11:18:53 +0200

    added PESEL validator tests

--- src/test/java/PESELValidatorTest.java
+++ src/test/java/PESELValidatorTest.java
@@ -1,2 +1,38 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class PESELValidatorTest {
+    private Validator PESELValidator = new PESELValidator();
+
+    @Test
+    public void test_lower_length() {
+        String PESEL = "1234567890";
+        assertFalse(PESELValidator.validate(PESEL));
+    }
+
+    @Test
+    public void test_higher_length() {
+        String PESEL = "123456789012";
+        assertFalse(PESELValidator.validate(PESEL));
+    }
+
+    @Test
+    public void test_PESEL_not_consisting_of_digits() {
+        String PESEL = "1234A567890";
+        assertFalse(PESELValidator.validate(PESEL));
+    }
+
+    @Test
+    public void test_correct_PESEL() {
+        String PESEL = "22060915917";
+        assertTrue(PESELValidator.validate(PESEL));
+    }
+
+    @Test
+    public void test_incorrect_PESEL() {
+        String PESEL = "22060915918";
+        assertTrue(PESELValidator.validate(PESEL));
+    }
 }
